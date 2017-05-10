fd1f23cd99ae9a83bfd143fc4b334dbe7046633f
Alexey Berdnikov <aberdnikov@gmail.com>
2017-05-10 14:52:26 +0500

    fixed

--- init.php
+++ init.php
@@ -2,5 +2,6 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-adminlte')
     ->setSourceDir('public')
     ->usePackage('larakit/sf-bootstrap')
+    ->usePackage('larakit/sf-font-awesome')
     ->cssPackage('css/AdminLTE.min.css')
     ->cssPackage('css/_all-skins.min.css');
