fea9a28ad618477043815d2fa6045dd0da66f22c
Alexey Berdnikov <aberdnikov@gmail.com>
2017-05-31 15:29:14 +0500

    fixed

--- init.php
+++ init.php
@@ -8,3 +8,29 @@
     ->cssPackage('css/_all-skins.min.css')
     ->cssPackage('css/larakit.css');
 \Larakit\Boot::register_boot(__DIR__ . '/boot');
+
+function adminlte_color_id($id) {
+    $colors = [
+        'bg-blue',
+//        'bg-gray-light',
+//        'bg-black',
+        'bg-red',
+        'bg-yellow',
+//        'bg-gray',
+        'bg-aqua',
+//        'bg-light-blue',
+        'bg-green',
+        'bg-navy',
+        'bg-teal',
+        'bg-olive',
+//        'bg-lime',
+        'bg-orange',
+        'bg-fuchsia',
+        'bg-purple',
+        'bg-maroon',
+        'bg-gray-active',
+        'bg-black-active',
+    ];
+    
+    return \Illuminate\Support\Arr::get($colors, (int) $id % count($colors));
+}
